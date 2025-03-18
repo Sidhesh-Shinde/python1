42b8c3c19100134dfeeab3eb0f65dfd22d5180f0
Sidhesh-Shinde <shinde.sidhesh@gmail.com>
2025-03-18 16:58:49 +0530

    Third Commit

--- file1.py
+++ file1.py
@@ -1,9 +1,1 @@
-def add(a, b):
-    return a + b
-
-# Example usage
-if __name__ == "__main__":
-    num1 = 5
-    num2 = 3
-    result = add(num1, num2)
-    print(f"The addition of {num1} and {num2} is {result}")
+print("Hello World")
